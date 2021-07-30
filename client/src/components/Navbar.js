37ec65c745ae95f1ca865d92916f2938efb24244
yash gupta <yash3144@gmail.com>
2021-07-30 17:14:39 +0530

    solve navbar dropdown issue

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,8 +13,8 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded">
                 <a className="navbar-brand" href="/">My PIZZA</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+                {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"> <span className="navbar-toggler-icon"></span></i>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
@@ -34,12 +34,34 @@ export default function Navbar() {
 
                         }
 
-                        <li className="nav-item">
+                        <li className="nav-item ms-auto">
                             <a className="nav-link" href="/cart">
                                 Cart({cartstate.cartItems.length})
                             </a>
                         </li>
                     </ul>
+                </div> */}
+                <div style={{display:'flex'}} class=" nav-item dropdown ms-auto">
+                    {
+                        currentUser != null ?
+                            (<div>
+                                <button style={{backgroundColor:'white',border:'none',outline:'none',marginTop:'6px'}} class=" dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    {currentUser.name}
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+
+                                    <li><a class="dropdown-item" href="/orders">Orders</a></li>
+                                    <li><a class="dropdown-item" href="#" onClick={() => dispatch(logoutUser())}>Logout</a></li>
+                                </ul>
+                            </div>)
+                            :
+                            (<div><li style={{listStyle:'none' , fontSize:'18px',marginTop:'2px',marginLeft:'15px'}}><a className="dropdown-item" href="/login">Login</a></li></div>)
+                    }
+                    <li style={{listStyle:'none'}} className="nav-item ms-auto">
+                        <a  className="nav-link" href="/cart">
+                            Cart({cartstate.cartItems.length})
+                        </a>
+                    </li>
                 </div>
             </nav>
         </div>
